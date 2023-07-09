11d3c570da96a78b5c167615435bc82109a46851
Ryan McTague <ryan.mctague@slope.io>
2023-07-09 08:54:01 -0400

    added base layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ryanmctague.github.io',
   vite: {
     plugins: [yaml()]
-  },
-  integrations: [mdx()]
+  }
 });
