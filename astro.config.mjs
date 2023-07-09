fd730e99c3dd4b4f46dfa4fb9876708e5730bb97
Ryan McTague <ryan.mctague@slope.io>
2023-07-09 08:09:39 -0400

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ryanmctague.github.io',
   vite:{
     plugins: [yaml()]
   }
--- src/files.d.ts
+++ src/files.d.ts
@@ -2,3 +2,8 @@ declare module '*.yaml' {
     const value: any // Add type definitions here if desired
     export default value
 }
+
+declare module '*.yml' {
+    const value: any // Add type definitions here if desired
+    export default value
+}
